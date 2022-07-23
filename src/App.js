4c7a035b4049387d3e0baae67048e3ee497019b0
danie11z <daniel.estudando1@gmail.com>
2022-07-23 14:23:31 -0300

    Counter by React JS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class Counter extends React.Component{
+  state = {
+    count: 0,
+  }
+
+  Add = () => {
+    if(this.state.count<10){
+      this.setState({
+        count: this.state.count +1
+      })
+    }
+  }
+
+  Remove = () => {
+    if(this.state.count>0){
+      this.setState({
+        count: this.state.count -1
+      })
+    }
+  }
+
+  render(){
+    return(
+      <>
+        <button onClick={this.Add}>+</button>
+        <p>{this.state.count}</p>
+        <button onClick={this.Remove}>-</button>
+      </>
+    )
+  }
 }
-
-export default App;
